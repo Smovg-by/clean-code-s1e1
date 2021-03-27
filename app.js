68eacacc78389a78e5a49d3933f2b2b96af9b358
Smovg-by <74701840+Smovg-by@users.noreply.github.com>
2021-03-27 18:51:08 +0300

    fix: refactor HTML,CSS&JS to BEM classes according to task extended rule 2.1

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ var createNewTaskElement=function(taskString){
     var deleteButtonImg=document.createElement("img");//delete button image
 
     label.innerText=taskString;
-    label.className='task';
+    label.className='task-container__item_label';
 
     //Each elements, needs appending
     checkBox.type="checkbox";
